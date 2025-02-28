5397fb9a5bf2e29879dbd0851953c3e931db7c52
Aislan Pepi <aislan.pepi@outlook.com>
2025-02-28 20:40:02 -0300

    OK

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -30,6 +30,7 @@ public class Main {
 
             switch (op) {
                 case 1:
+                    em.getTransaction().begin();
                     Aluno aluno = new Aluno();
                     System.out.println("CADASTRO DE ALUNO");
                     System.out.print("Digite o nome: ");
@@ -60,104 +61,96 @@ public class Main {
                     }
                     aluno.setNota3(n);
 
-                    em.getTransaction().begin();
                     dao.cadastrar(aluno);
                     em.getTransaction().commit();
-
                     break;
 
                 case 2:
+                    em.getTransaction().begin();
                     System.out.println("EXCLUIR ALUNO:");
                     System.out.print("Digite o nome: ");
                     String nome = sc.nextLine();
-
-                    em = JPAUtil.getEntityManager();
-                    dao = new AlunoDAO(em);
-                    em.getTransaction().begin();
-                    dao.deletar(nome);
                     try {
+                        dao.deletar(nome);
                         em.getTransaction().commit();
                         System.out.println("Aluno removido com sucesso!");
-                    } catch (Exception e) {
-                        System.err.println("Ocorreu algum erro!");
+                    } catch (Exception e){
+                        System.err.println("Aluno nao encontrado!");
+                        em.getTransaction().rollback();
                     }
                     break;
 
                 case 3:
+                    em.getTransaction().begin();
                     System.out.println("ALTERAR ALUNO:");
                     System.out.print("Digite o nome: ");
                     nome = sc.nextLine();
 
-                    dao = new AlunoDAO(em);
-                    em.getTransaction().begin();
-                    aluno = dao.buscarPorNome(nome);
+                    try {
+                        aluno = dao.buscarPorNome(nome);
+                        em.persist(aluno);
 
-                    em.persist(aluno);
+                        System.out.println("Dados do Aluno(a)");
+                        System.out.printf("Nome: %s\n", aluno.getNome());
+                        System.out.printf("Email: %s\n", aluno.getEmail());
+                        System.out.printf("RA: %s\n", aluno.getRa());
+                        System.out.printf("Notas: %.2f - %.2f - %.2f\n", aluno.getNota1(), aluno.getNota2(), aluno.getNota3());
 
-                    System.out.println("Dados do Aluno(a)");
-                    System.out.printf("Nome: %s\n",aluno.getNome());
-                    System.out.printf("Email: %s\n",aluno.getEmail());
-                    System.out.printf("RA: %s\n",aluno.getRa());
-                    System.out.printf("Notas: %.2f - %.2f - %.2f\n",aluno.getNota1(),aluno.getNota2(),aluno.getNota3());
-
-                    System.out.println("Novos dados:");
-                    System.out.print("Digite o nome: ");
-                    aluno.setNome(sc.nextLine());
-                    System.out.print("Digite o RA: ");
-                    aluno.setRa(sc.nextLine());
-                    System.out.print("Digite o email: ");
-                    aluno.setEmail(sc.nextLine());
-                    System.out.print("Digite a nota 1: ");
-                    aluno.setNota1(BigDecimal.valueOf(Long.parseLong(sc.nextLine())));
-                    System.out.print("Digite a nota 2: ");
-                    aluno.setNota2(BigDecimal.valueOf(Long.parseLong(sc.nextLine())));
-                    System.out.print("Digite a nota 3: ");
-                    aluno.setNota3(BigDecimal.valueOf(Long.parseLong(sc.nextLine())));
-
-                    em.getTransaction().commit();
-
+                        System.out.println("Novos dados:");
+                        System.out.print("Digite o nome: ");
+                        aluno.setNome(sc.nextLine());
+                        System.out.print("Digite o RA: ");
+                        aluno.setRa(sc.nextLine());
+                        System.out.print("Digite o email: ");
+                        aluno.setEmail(sc.nextLine());
+                        System.out.print("Digite a nota 1: ");
+                        aluno.setNota1(BigDecimal.valueOf(Double.parseDouble(sc.nextLine())));
+                        System.out.print("Digite a nota 2: ");
+                        aluno.setNota2(BigDecimal.valueOf(Double.parseDouble(sc.nextLine())));
+                        System.out.print("Digite a nota 3: ");
+                        aluno.setNota3(BigDecimal.valueOf(Double.parseDouble(sc.nextLine())));
+                        em.getTransaction().commit();
+                        System.out.println("Aluno alterado com sucesso!");
+                    } catch (Exception e){
+                        System.err.println("Aluno não encontrado!");
+                        em.getTransaction().rollback();
+                    }
                     break;
 
                 case 4:
-
                     System.out.println("CONSULTAR ALUNO");
                     System.out.print("Digite o nome: ");
                     nome = sc.nextLine();
-
-                    dao = new AlunoDAO(em);
-                    em.getTransaction().begin();
                     try {
                         aluno = dao.buscarPorNome(nome);
-
                         System.out.println("Dados do Aluno(a)");
-                        System.out.printf("Nome: %s\n",aluno.getNome());
-                        System.out.printf("Email: %s\n",aluno.getEmail());
-                        System.out.printf("RA: %s\n",aluno.getRa());
-                        System.out.printf("Notas: %.2f - %.2f - %.2f\n",aluno.getNota1(),aluno.getNota2(),aluno.getNota3());
-                    } catch (NoResultException e){
+                        System.out.printf("Nome: %s\n", aluno.getNome());
+                        System.out.printf("Email: %s\n", aluno.getEmail());
+                        System.out.printf("RA: %s\n", aluno.getRa());
+                        System.out.printf("Notas: %.2f - %.2f - %.2f\n", aluno.getNota1(), aluno.getNota2(), aluno.getNota3());
+                    } catch (NoResultException e) {
                         System.err.println("Aluno nao encontrado!");
                     }
-
-                    em.getTransaction().commit();
-
                     break;
-                    
+
                 case 5:
                     System.out.println("Exibindo todos os alunos:");
                     List<Aluno> alunos = dao.buscarTodos();
 
                     for(Aluno a : alunos){
                         double media = (a.getNota1().doubleValue() + a.getNota2().doubleValue() + a.getNota3().doubleValue()) / 3;
-                        System.out.printf("\nNome: %s\n",a.getNome());
-                        System.out.printf("Email: %s\n",a.getEmail());
-                        System.out.printf("RA: %s\n",a.getRa());
-                        System.out.printf("Notas: %.2f - %.2f - %.2f\n",a.getNota1(),a.getNota2(),a.getNota3());
-                        System.out.printf("Media: %.2f\n",media);
-                        if(media >= 6) System.out.print("Situação: Aprovado\n");
-                        if(media < 4) System.out.print("Situação: Reprovado\n");
-                        if(media >= 4 && media < 6) System.out.print("Situação: Recuperação\n");
+                        System.out.printf("\nNome: %s\n", a.getNome());
+                        System.out.printf("Email: %s\n", a.getEmail());
+                        System.out.printf("RA: %s\n", a.getRa());
+                        System.out.printf("Notas: %.2f - %.2f - %.2f\n", a.getNota1(), a.getNota2(), a.getNota3());
+                        System.out.printf("Media: %.2f\n", media);
+                        if (media >= 6) System.out.print("Situação: Aprovado\n");
+                        if (media < 4) System.out.print("Situação: Reprovado\n");
+                        if (media >= 4 && media < 6) System.out.print("Situação: Recuperação\n");
                     }
+                    break;
             }
         }
+        em.close();
     }
 }
